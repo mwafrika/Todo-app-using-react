0e26d5e2bf7c0ee1d3b34bff8ad904b03bf1c7b9
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-03-22 21:32:57 +0200

    Fix linters errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './todo/header';
 import Home from './todo/index';
 import About from './about/about';
 import Contact from './contact/contact';
@@ -11,23 +10,10 @@ export default function App() {
     <Router>
       <Routes>
         <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='*' element={<NoPage />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </Router>
-    // <Router>
-    //   <div>
-    //     <Route exact path='/'>
-    //       <Home />
-    //     </Route>
-    //     <Route path='/about'>
-    //       <About />
-    //     </Route>
-    //     <Route path='/news'>
-    //       <Contact />
-    //     </Route>
-    //   </div>
-    // </Router>
   );
 }
